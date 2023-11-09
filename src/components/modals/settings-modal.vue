d78e956c59f5098c4027e047125d687c1f72f1ed
VChet <17050347+VChet@users.noreply.github.com>
2023-11-10 01:02:32 +0400

    feat(settings): add token instructions

--- src/components/modals/settings-modal.vue
+++ src/components/modals/settings-modal.vue
@@ -3,15 +3,26 @@
     <icon-settings />
     Settings
   </button>
-  <dialog ref="dialogRef">
+  <dialog ref="dialogRef" class="settings">
     <header>Settings</header>
-    <form class="settings-form" @submit.prevent="update">
+    <form class="settings__form" @submit.prevent="update">
       <label>
         <input v-model="showOwner" type="checkbox" />
         Show repository owner
       </label>
       <label for="authToken">GitHub Token</label>
       <textarea id="authToken" v-model.trim="authTokenInput" placeholder="authToken" />
+      <ol>
+        <li>
+          Go to
+          <a href="https://github.com/settings/tokens?type=beta" title="github tokens page">
+            github.com/settings/tokens
+          </a>
+        </li>
+        <li>Click "Generate new token"</li>
+        <li>Give access to repository metadata and contents</li>
+        <li>Click "Generate Token" and paste it here</li>
+      </ol>
       <button title="set auth token" type="submit" :disabled="!authTokenInput">Update</button>
     </form>
   </dialog>
@@ -34,9 +45,12 @@ async function update() {
 }
 </script>
 <style lang="scss">
-.settings-form {
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
+.settings {
+  max-width: 400px;
+  &__form {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
 }
 </style>
