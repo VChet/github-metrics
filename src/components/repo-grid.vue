b871f879af63f7fead6b9fea03288efa7b56a5ee
VChet <17050347+VChet@users.noreply.github.com>
2024-01-07 01:58:13 +0400

    fix(repo-grid): add basic instructions to empty state

--- src/components/repo-grid.vue
+++ src/components/repo-grid.vue
@@ -1,7 +1,14 @@
 <template>
   <section class="repo-grid">
     <div v-if="!storage.repositories.length" class="repo-grid__placeholder">
-      No repos
+      <b>No repositories added yet.</b>
+      <p>
+        To&nbsp;add a&nbsp;repository manually, use the &laquo;Add GitHub repo&raquo; button above and enter it's owner and name.
+      </p>
+      <p>
+        Alternatively, visit &laquo;Settings&raquo; to&nbsp;add your token and then press &laquo;Add GitHub repo&raquo;,
+        where you could selectively choose repositories to&nbsp;add.
+      </p>
     </div>
     <template v-else>
       <div class="repo-grid__filters">
