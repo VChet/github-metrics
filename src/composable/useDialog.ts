9ca16615abbfbe9ce21800279fb192c1aaeaa1d7
VChet <17050347+VChet@users.noreply.github.com>
2024-02-01 21:46:06 +0400

    fix(dialog): add scrollbar

--- src/composable/useDialog.ts
+++ src/composable/useDialog.ts
@@ -6,6 +6,7 @@ export function useDialog(elementRef?: Ref<HTMLDialogElement | null>) {
   function open() {
     if (!element.value) return;
     element.value.showModal();
+    document.body.classList.add("disable-scroll");
   }
   function close() {
     if (!element.value) return;
@@ -13,7 +14,10 @@ export function useDialog(elementRef?: Ref<HTMLDialogElement | null>) {
   }
   // Close on click outside
   useEventListener(element, "mousedown", (event) => {
-    if (event.target === element.value) element.value?.close();
+    if (event.target === element.value) {
+      element.value?.close();
+      document.body.classList.remove("disable-scroll");
+    }
   });
 
   return { element, open, close };
