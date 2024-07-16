e9acba1674a316c88b1f930cfb4953c19e011e09
VChet <17050347+VChet@users.noreply.github.com>
2024-07-17 03:25:24 +0500

    chore(env): remove *.vue declaration

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,1 @@
 /// <reference types="vite/client" />
-
-declare module "*.vue" {
-  import type { ComponentOptions } from "vue";
-
-  const Component: ComponentOptions;
-  export default Component;
-}
