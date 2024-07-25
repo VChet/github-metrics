8a91084caf6f3d378ee74b66d080fd75a6d74499
VChet <17050347+VChet@users.noreply.github.com>
2024-07-25 23:01:44 +0500

    refactor(App): move code to components, add wrapper

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,23 @@
 <template>
-  <main-header class="container" />
-  <user-feed v-if="isFeedAvailable" class="container" />
-  <div v-if="!storage.repositories.length" class="container repo-grid__placeholder">
-    <b>No repositories added yet.</b>
-    <p>
-      To&nbsp;add a&nbsp;repository manually, use the &laquo;Add GitHub repo&raquo; button above and enter it's owner and name.
-    </p>
-    <p>
-      Alternatively, visit &laquo;Settings&raquo; to&nbsp;add your token and then press &laquo;Add GitHub repo&raquo;,
-      where you could selectively choose repositories to&nbsp;add.
-    </p>
-  </div>
-  <template v-else>
-    <ul class="container tab-selector">
-      <li v-for="{ value, text } in tabs" :key="value">
-        <button
-          class="tab-selector__button"
-          :class="{ 'tab-selector__button--active': selectedTab === value }"
-          type="button"
-          @click="selectedTab = value"
-        >
-          {{ text }}
-        </button>
-      </li>
-    </ul>
-    <repo-grid v-show="selectedTab === 'grid'" class="container" />
-    <dependencies-table v-show="selectedTab === 'table'" class="container" />
-  </template>
+  <main class="container main-container">
+    <main-header />
+    <user-feed v-if="isFeedAvailable" />
+    <main-placeholder v-if="!storage.repositories.length" />
+    <template v-else>
+      <tab-selector v-model="selectedTab" :items="tabs" />
+      <repo-grid v-show="selectedTab === 'grid'" />
+      <dependencies-table v-show="selectedTab === 'table'" />
+    </template>
+  </main>
 </template>
 <script setup lang="ts">
 import { computed, ref } from "vue";
-import MainHeader from "@/modules/header/main-header.vue";
-import UserFeed from "@/modules/feed/user-feed.vue";
-import RepoGrid from "@/modules/grid/repo-grid.vue";
 import DependenciesTable from "@/modules/dependencies/dependencies-table.vue";
+import MainHeader from "@/modules/header/main-header.vue";
+import MainPlaceholder from "@/modules/main-placeholder.vue";
+import RepoGrid from "@/modules/grid/repo-grid.vue";
+import TabSelector from "@/modules/tab-selector.vue";
+import UserFeed from "@/modules/feed/user-feed.vue";
 import { useSettingsStore } from "@/store/settings";
 import { useRepositoriesStore } from "@/store/repositories";
 
@@ -48,15 +32,8 @@ const tabs = [
 const selectedTab = ref(tabs[0].value);
 </script>
 <style lang="scss">
-.tab-selector {
-  display: flex;
-  gap: .5rem;
-  align-items: center;
-  &__button {
-    &--active {
-      color: var(--accent);
-      border-color: var(--accent);
-    }
-  }
+.main-container {
+  display: grid;
+  gap: 1.5rem;
 }
 </style>
