22fe70765b2d26f39e2f14128e199de1cd3c1318
VChet <17050347+VChet@users.noreply.github.com>
2024-07-25 23:24:10 +0500

    feat(about): add last update date

--- src/modules/header/modals/about-modal.vue
+++ src/modules/header/modals/about-modal.vue
@@ -11,26 +11,34 @@
           <icon-x />
         </button>
       </header>
-      <div>
-        <p>
-          Not affiliated with GitHub.
-        </p>
-        <p>
-          All data is stored locally in your browser's localStorage.
-        </p>
-        <p>
-          <a href="https://github.com/VChet/github-metrics">
-            <icon-brand-github />
-            Source code
-          </a>
-        </p>
+      <div class="about__content">
+        <div>Not affiliated with GitHub.</div>
+        <div>All data is stored locally in your browser's localStorage.</div>
+        <a href="https://github.com/VChet/github-metrics">
+          <icon-brand-github />
+          Source code
+        </a>
+        <div>
+          Last update: {{ commitDate }}
+        </div>
       </div>
     </dialog>
   </teleport>
 </template>
 <script setup lang="ts">
 import { IconBrandGithub, IconTilde, IconX } from "@tabler/icons-vue";
+import dayjs from "dayjs";
 import { useDialog } from "@/composable/useDialog";
 
 const { element: dialogRef, open, close } = useDialog();
+const commitDate = dayjs(import.meta.env.VITE_GIT_COMMIT_DATE).format("DD.MM.YY");
 </script>
+<style lang="scss">
+.about {
+  max-width: 40vw;
+  &__content {
+    display: grid;
+    gap: 0.5rem;
+  }
+}
+</style>
