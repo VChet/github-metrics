e449e10d434b9b283fb080a3eef6b514a4da0011
VChet <17050347+VChet@users.noreply.github.com>
2024-07-25 23:24:10 +0500

    feat(about): add last update date

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { URL, fileURLToPath } from "node:url";
+import { execSync } from "node:child_process";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+
+const commitDate = execSync("git log -1 --format=%cI").toString().trimEnd();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +12,8 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
     }
+  },
+  define: {
+    "import.meta.env.VITE_GIT_COMMIT_DATE": JSON.stringify(commitDate)
   }
 });
