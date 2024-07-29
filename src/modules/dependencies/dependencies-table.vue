19503e35ad46653b86bdbbff1176779fec56500d
VChet <17050347+VChet@users.noreply.github.com>
2024-07-30 01:15:58 +0500

    feat(dependencies): highlight version diff

--- src/modules/dependencies/dependencies-table.vue
+++ src/modules/dependencies/dependencies-table.vue
@@ -28,12 +28,13 @@
           <td :title="dep" :style="{ color: composeHashColorFromString(dep) }" class="chip">
             <a :href="`https://npmjs.org/${dep}`" target="_blank" class="text-truncate">
               {{ dep }}
+              <div>{{ latestVersions[dep] }}</div>
             </a>
             <button class="icon" type="button" title="exclude dependency" @click="hideDependency(dep)">
               <icon-square-x />
             </button>
           </td>
-          <td v-for="repo in repos" :key="repo.id">
+          <td v-for="repo in repos" :key="repo.id" :class="versionDiffClass(dep, repo.dependencies![dep])">
             {{ repo.dependencies![dep] }}
           </td>
         </tr>
@@ -42,8 +43,9 @@
   </div>
 </template>
 <script setup lang="ts">
-import { useStorage } from "@vueuse/core";
+import { computedAsync, useMemoize, useStorage } from "@vueuse/core";
 import { IconSquareX } from "@tabler/icons-vue";
+import semverDiff from "semver/functions/diff";
 import { useDependencyTable } from "@/composable/useDependencyTable";
 import { useSettingsStore } from "@/store/settings";
 import { composeHashColorFromString } from "@/composable/useLibColor";
@@ -58,10 +60,36 @@ function hideDependency(dep: string): void {
 function showDependency(dep: string): void {
   excludedDependencies.value.delete(dep);
 }
+
+const fetchLatestVersion = useMemoize(async (dependency: string) => {
+  const response = await fetch(`https://registry.npmjs.org/${dependency}/latest`);
+  const data = await response.json();
+  return data.version;
+});
+
+const latestVersions = computedAsync(() => {
+  const map: Record<string, string> = {};
+  dependencies.value.forEach(async (dependency) => {
+    if (!map[dependency]) { map[dependency] = await fetchLatestVersion(dependency); }
+  });
+  return map;
+}, {}, { lazy: true });
+
+function versionDiffClass(packageName: string, version?: string) {
+  if (!version) return null;
+  const projectVersion = version.replace(/[^0-9.]/g, "");
+  const latestVersion = latestVersions.value[packageName];
+  if (!latestVersion) return null;
+  const diff = semverDiff(projectVersion, latestVersion);
+  return diff ?? null;
+}
 </script>
 <style lang="scss">
 .dependencies {
   overflow: auto;
+  fieldset {
+    margin-bottom: 1rem;
+  }
   &__excluded {
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
@@ -85,11 +113,28 @@ function showDependency(dep: string): void {
     cursor: default;
     border-spacing: 0;
     border-collapse: collapse;
+    td {
+      &.patch {
+        color: mediumseagreen;
+      }
+      &.minor {
+        color: darkcyan;
+      }
+      &.major {
+        color: crimson;
+      }
+    }
     th, td {
       padding: 0.25rem;
       white-space: nowrap;
       &:first-of-type {
-        max-width: 10rem;
+        max-width: 12vw;
+        div {
+          font-size: 0.75rem;
+        }
+        @media (width <= 600px) {
+          max-width: 10rem;
+        }
       }
       &:not(:first-of-type) {
         text-align: center;
