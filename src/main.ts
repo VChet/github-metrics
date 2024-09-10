542557a1e70a57fa12478d20113aeecd9ac21529
VChet <17050347+VChet@users.noreply.github.com>
2024-09-10 22:14:42 +0500

    chore(eslint): update sorting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
 import VueDOMPurifyHTML from "vue-dompurify-html";
 import router from "./router";
-import App from "./App.vue";
 import "@/assets/global.scss";
+import App from "./App.vue";
 
 createApp(App)
   .use(router)
