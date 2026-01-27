8190350c5b5fa62302579efafbab59242d461dbf
VChet <17050347+VChet@users.noreply.github.com>
2026-01-27 09:40:50 +0500

    chore: update dependencies

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,6 +5,7 @@ export default {
   ],
   plugins: [
     "@stylistic/stylelint-plugin",
+    "stylelint-declaration-block-no-ignored-properties",
     "stylelint-order",
     "stylelint-rem-over-px"
   ],
@@ -22,6 +23,7 @@ export default {
     "@stylistic/color-hex-case": "lower",
     "at-rule-empty-line-before": "never",
     "declaration-empty-line-before": "never",
+    "plugin/declaration-block-no-ignored-properties": true,
     "rem-over-px/rem-over-px": [true, { ignore: ["1px", "font-size 16px"] }],
     "rule-empty-line-before": "never",
     "selector-class-pattern": "[a-z]([a-z-]+)?(__([a-z]+-?)+)?(--([a-z]+-?)+){0,2}"
