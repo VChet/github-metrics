f5eb12d007e723258fe9d0f8a9b7ff3c1a84c615
VChet <17050347+VChet@users.noreply.github.com>
2026-03-22 14:04:23 +0500

    feat(color): update colors

--- src/helpers/color.ts
+++ src/helpers/color.ts
@@ -4,29 +4,33 @@ function getHsla(hue: number, opacity: number | string = 1): string {
 
 const predefinedColorMap: Readonly<Record<string, string>> = {
   "@octokit": "#ffffff",
-  "@types/": "#0086e0",
+  "@stylistic": "#f43f5e",
+  "@types": "#0086e0",
   "cspell": "#e67a64",
+  "es-toolkit": "#3dd68c",
   "esbuild": "#ffcf00",
   "eslint": "#b7b7ff",
   "neostandard": "#b7b7ff",
   "postcss": "#f44d27",
   "sass": "#cc6699",
   "scss": "#cc6699",
+  "storybook": "#ff4785",
   "stylelint": "#d0d5dd",
   "svelte": "#ff3e00",
-  "ts-custom-error": "#0086e0",
-  "tsconfig": "#0086e0",
-  "tsdown": "#0086e0",
-  "tslib": "#0086e0",
-  "tsx": "#0086e0",
-  "type-fest": "#0086e0",
-  "typescript": "#0086e0",
+  "ts": "#0086e0",
+  "type": "#0086e0",
   "vite": "#9499ff",
   "vue": "#41b883",
   "workbox": "#ee810f"
 };
 
+function normalizeName(name: string): string {
+  if (name.startsWith("@")) return name.split("/")[0];
+  return name;
+}
+
 export function composeHashColor(name: string): string {
+  name = normalizeName(name);
   const mapKey = Object.keys(predefinedColorMap).find((key) => name.includes(key));
   if (mapKey) return predefinedColorMap[mapKey];
 
